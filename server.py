950c4a14f1431626cdd9c1671efaa965e63a2eb6
pulkitadua <pdua@andrew.cmu.edu>
2015-02-21 14:33:04 -0500

    Pulkita Testing

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'This is Opti. <br> <img style="width:250px" src="http://www.clker.com/cliparts/r/o/x/t/p/y/purple-postit-hi.png">'
+    return 'This is Opti! <br> <img style="width:250px" src="http://www.clker.com/cliparts/r/o/x/t/p/y/purple-postit-hi.png">'
