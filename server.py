764cead6fec09ea5786adce5fceb28b0eec9164f
cvsmith <cvsmith@andrew.cmu.edu>
2015-02-21 15:48:26 -0500

    Attempt linking index and server

--- server.py
+++ server.py
@@ -4,5 +4,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'This is Opti! <br> <img style="width:250px" src="http://www.clker.com/cliparts/r/o/x/t/p/y/purple-postit-hi.png"><br> Coming soon!'
+def index():
+    return render_template('index.html')
