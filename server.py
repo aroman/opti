a61ff7a11c630a5f41f5d53ef5a043a8ca94e206
Avi Romanoff <avi@romanoff.me>
2015-02-21 02:53:07 -0500

    vanity commit

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<img src="http://www.clker.com/cliparts/r/o/x/t/p/y/purple-postit-hi.png">'
+    return '<img style="width:250px" src="http://www.clker.com/cliparts/r/o/x/t/p/y/purple-postit-hi.png">'
